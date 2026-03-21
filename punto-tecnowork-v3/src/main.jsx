b977dda0281a2f381b4814daea96f8be5946da48
powerpuntotw <powerpuntotw@gmail.com>
2026-03-21 11:16:36 -0300

    feat: agregar ThemeProvider en main.jsx

--- punto-tecnowork-v3/src/main.jsx
+++ punto-tecnowork-v3/src/main.jsx
@@ -3,17 +3,20 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import { AuthProvider } from './context/AuthContext'
 import { BrandingProvider } from './context/BrandingContext'
+import { ThemeProvider } from './context/ThemeContext'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <BrandingProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </BrandingProvider>
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <BrandingProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </BrandingProvider>
+      </AuthProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
